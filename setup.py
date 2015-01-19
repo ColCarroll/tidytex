914ca60bcab0eda91c12a5f0a6851172b5dd9a73
colcarroll <colcarroll@gmail.com>
2015-01-18 21:56:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup
 setup(
     name='tidytex',
     version='0.1',
+    description='Compile LaTeX with no auxiliary files',
+    long_description='''
+    Continuously compile LaTeX, keeping auxiliary files in a temporary
+    folder, which is destroyed when the process is killed.
+    ''',
+    author='Colin Carroll',
     py_modules=['tidytex'],
     include_package_data=True,
     install_requires=[
